f5e6e8ae4ba17d8e6dd5e030c6189d7aa8a51656
Administrator <lql4@student.unimelb.edu.au>
2022-04-26 11:26:37 +1000

    ansible: install env dependencies/ remove all function

--- Ansible/run_create_instance.sh
+++ Ansible/run_create_instance.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./unimelb-COMP90024-2022-grp-12-openrc.sh; ansible-playbook --key-file ./config/mrcSSH.pem create_instance.yaml
+./unimelb-COMP90024-2022-grp-12-openrc.sh; ansible-playbook create_instance.yaml
