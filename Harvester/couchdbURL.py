2666cdb3bdee4c5c20edff3bcc5d1d26c60f11d9
QiLiJazzBebop <82303079+QiLiJazzBebop@users.noreply.github.com>
2022-05-17 09:58:53 +1000

    Deploy harvester

--- Harvester/couchdbURL.py
+++ Harvester/couchdbURL.py
@@ -1,2 +1,7 @@
 def returnURL():
-    return "172.26.134.245:5984/"
+    adminName = "admin"
+    adminPasswd = "adminPass"
+    url = "172.26.134.245:5984/"
+    # define couch
+    couch_url = "http://" + adminName + ":" + adminPasswd + "@" + url
+    return couch_url
